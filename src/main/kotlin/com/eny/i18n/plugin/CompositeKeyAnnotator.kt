836527d416e2d845983e1f1053e9a600856906db
nyavro <e.nyavro@gmail.com>
2019-09-08 17:26:13 +0300

    Fixed annotations of fully resolved keys

--- src/main/kotlin/com/eny/i18n/plugin/CompositeKeyAnnotator.kt
+++ src/main/kotlin/com/eny/i18n/plugin/CompositeKeyAnnotator.kt
@@ -1,90 +1,10 @@
 package com.eny.i18n.plugin
 
-import com.eny.i18n.plugin.utils.CompositeKeyResolver
-import com.eny.i18n.plugin.utils.I18nKeyLiteral
-import com.eny.i18n.plugin.utils.JavaScriptUtil
-import com.eny.i18n.plugin.utils.JsonSearchUtil
+import com.eny.i18n.plugin.utils.*
 import com.intellij.json.psi.JsonStringLiteral
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-
-/**
- * Annotation helper methods
- */
-class AnnotationHelper(val element: PsiElement, val holder: AnnotationHolder) {
-    private val RESOLVED_COLOR = DefaultLanguageHighlighterColors.LINE_COMMENT
-    fun annotateResolved(fileName: String) {
-        holder.createInfoAnnotation(
-            TextRange(
-                element.textRange.startOffset + fileName.length + 1,
-                element.textRange.endOffset
-            ), null
-        ).textAttributes = RESOLVED_COLOR
-    }
-    fun annotateReferenceToJson(fullKey: I18nFullKey) {
-        holder.createErrorAnnotation(
-            TextRange (
-                    compositeKeyStartOffset(fullKey),
-                    element.textRange.endOffset - 1
-            ),
-            "Reference to Json object"
-        )
-    }
-    fun annotateReferenceToPlural(fullKey: I18nFullKey) {
-        holder.createInfoAnnotation(
-            TextRange (
-                compositeKeyStartOffset(fullKey),
-                element.textRange.endOffset - 1
-            ),"Reference to plural value"
-        ).textAttributes = RESOLVED_COLOR
-    }
-    fun annotateUnresolved(fullKey: I18nFullKey, resolvedPath: List<String>) {
-        val compositeKeyStartOffset = compositeKeyStartOffset(fullKey)
-        val unresolvedStartOffset = compositeKeyStartOffset +
-                resolvedPathLength(resolvedPath)
-        val unresolvedEndOffset = element.textRange.endOffset - 1
-        holder.createErrorAnnotation(
-            TextRange (
-                    unresolvedStartOffset,
-                    unresolvedEndOffset
-            ),
-            "Unresolved property"
-        )//.registerFix(CreatePropertyQuickFix(fullKey))
-    }
-    fun annotatePartiallyResolved(fullKey: I18nFullKey, resolvedPath: List<String>) {
-        val compositeKeyStartOffset = compositeKeyStartOffset(fullKey)
-        val unresolvedStartOffset = compositeKeyStartOffset +
-                resolvedPathLength(resolvedPath)
-        holder.createInfoAnnotation (
-                TextRange (
-                        compositeKeyStartOffset,
-                        unresolvedStartOffset
-                ),
-                "Partially resolved"
-        ).textAttributes = RESOLVED_COLOR
-    }
-
-    private fun resolvedPathLength(resolvedPath: List<String>) =
-            if (resolvedPath.isEmpty()) 0
-            else resolvedPath.joinToString(I18nFullKey.CompositeKeySeparator, postfix = I18nFullKey.CompositeKeySeparator).length
-
-    private fun compositeKeyStartOffset(fullKey: I18nFullKey): Int {
-        return element.textRange.startOffset +
-            (fullKey.fileName?.let { name -> name.length + I18nFullKey.FileNameSeparator.length } ?: 0) + 1
-    }
-
-    fun annotateFileUnresolved(fileName: String, text: String) {
-        holder.createErrorAnnotation(
-            TextRange (
-                element.textRange.startOffset + 1,
-                element.textRange.startOffset + fileName.length + 1
-            ), text
-        )//.registerFix(CreateJsonFileQuickFix(fileName))
-    }
-}
 
 /**
  * Annotator for i18n keys
@@ -102,9 +22,9 @@ class CompositeKeyAnnotator : Annotator, CompositeKeyResolver {
     private fun annotateI18nLiteral(value: I18nKeyLiteral, element: PsiElement, holder: AnnotationHolder) {
         val fullKey = I18nFullKey.parse(value.literal)
         if (fullKey?.fileName != null) {
-            val annotationHelper = AnnotationHelper(element, holder)
+            val annotationHelper = AnnotationHelper(element.textRange, holder)
             val files = JsonSearchUtil(element.project).findFilesByName(fullKey.fileName)
-            if (files.isEmpty()) annotationHelper.annotateFileUnresolved(fullKey.fileName, "Unresolved file")
+            if (files.isEmpty()) annotationHelper.annotateFileUnresolved(fullKey.fileName)
             else {
                 val mostResolvedReference = files
                         .flatMap { jsonFile ->
---
+++ src/main/kotlin/com/eny/i18n/plugin/utils/AnnotationHelper.kt
@@ -0,0 +1,81 @@
+package com.eny.i18n.plugin.utils
+
+import com.eny.i18n.plugin.I18nFullKey
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.util.TextRange
+
+/**
+ * Annotation helper methods
+ */
+class AnnotationHelper(val textRange: TextRange, val holder: AnnotationHolder) {
+    private val RESOLVED_COLOR = DefaultLanguageHighlighterColors.LINE_COMMENT
+    fun annotateResolved(fileName: String) {
+        holder.createInfoAnnotation(
+            TextRange(
+                textRange.startOffset + fileName.length + 1,
+                textRange.endOffset - 1
+            ), null
+        ).textAttributes = RESOLVED_COLOR
+    }
+    fun annotateReferenceToJson(fullKey: I18nFullKey) {
+        holder.createErrorAnnotation(
+            TextRange(
+                compositeKeyStartOffset(fullKey),
+                textRange.endOffset - 1
+            ),
+            "Reference to Json object"
+        )
+    }
+    fun annotateReferenceToPlural(fullKey: I18nFullKey) {
+        holder.createInfoAnnotation(
+            TextRange(
+                compositeKeyStartOffset(fullKey),
+                textRange.endOffset - 1
+            ),"Reference to plural value"
+        ).textAttributes = RESOLVED_COLOR
+    }
+    fun annotateUnresolved(fullKey: I18nFullKey, resolvedPath: List<String>) {
+        val compositeKeyStartOffset = compositeKeyStartOffset(fullKey)
+        val unresolvedStartOffset = compositeKeyStartOffset +
+                resolvedPathLength(resolvedPath)
+        val unresolvedEndOffset = textRange.endOffset - 1
+        holder.createErrorAnnotation(
+            TextRange(
+                unresolvedStartOffset,
+                unresolvedEndOffset
+            ),
+            "Unresolved property"
+        )//.registerFix(CreatePropertyQuickFix(fullKey))
+    }
+    fun annotatePartiallyResolved(fullKey: I18nFullKey, resolvedPath: List<String>) {
+        val compositeKeyStartOffset = compositeKeyStartOffset(fullKey)
+        val unresolvedStartOffset = compositeKeyStartOffset +
+                resolvedPathLength(resolvedPath)
+        holder.createInfoAnnotation (
+            TextRange(
+                compositeKeyStartOffset,
+                unresolvedStartOffset
+            ),
+                "Partially resolved"
+        ).textAttributes = RESOLVED_COLOR
+    }
+
+    fun annotateFileUnresolved(fileName: String) {
+        holder.createErrorAnnotation(
+            TextRange(
+                textRange.startOffset + 1,
+                textRange.startOffset + fileName.length + 1
+            ), "Unresolved file"
+        )//.registerFix(CreateJsonFileQuickFix(fileName))
+    }
+
+    private fun resolvedPathLength(resolvedPath: List<String>) =
+        if (resolvedPath.isEmpty()) 0
+        else resolvedPath.joinToString(I18nFullKey.CompositeKeySeparator, postfix = I18nFullKey.CompositeKeySeparator).length
+
+    private fun compositeKeyStartOffset(fullKey: I18nFullKey): Int {
+        return textRange.startOffset +
+            (fullKey.fileName?.let { name -> name.length + I18nFullKey.FileNameSeparator.length } ?: 0) + 1
+    }
+}
