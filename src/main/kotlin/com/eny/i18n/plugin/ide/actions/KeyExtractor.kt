77632d52c5b37526fccfba55aaa4d8e67293ab5f
nyavro <e.nyavro@gmail.com>
2020-04-07 19:29:37 +0300

    Key creation quick fix test introduced

--- src/main/kotlin/com/eny/i18n/plugin/ide/actions/KeyExtractor.kt
+++ src/main/kotlin/com/eny/i18n/plugin/ide/actions/KeyExtractor.kt
@@ -30,7 +30,7 @@ class KeyExtractor {
                 ContentGeneratorAdapter(YamlContentGenerator(), YamlPsiContentGenerator()),
                 ContentGeneratorAdapter(JsonContentGenerator(), JsonPsiContentGenerator())
             )
-            CreateKeyQuickFix(i18nKey, UserChoice(),  PluginBundle.getMessage("quickfix.create.property"), generators, source)
+            CreateKeyQuickFix(i18nKey, UserChoice(),  PluginBundle.getMessage("quickfix.create.key"), generators, source)
         }
         quickFix.invoke(project, editor)
     }
