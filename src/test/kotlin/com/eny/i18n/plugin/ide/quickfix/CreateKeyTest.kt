7af1e3cd1f75e45b2e37fbc753a690839f21ba15
nyavro <e.nyavro@gmail.com>
2020-04-08 21:30:33 +0300

    Create key quick fix test for yaml files

--- src/test/kotlin/com/eny/i18n/plugin/ide/quickfix/CreateKeyTest.kt
+++ src/test/kotlin/com/eny/i18n/plugin/ide/quickfix/CreateKeyTest.kt
@@ -2,25 +2,28 @@ package com.eny.i18n.plugin.ide.quickfix
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
-internal class CreateKeyTest: BasePlatformTestCase() {
-
+internal abstract class CreateKeyTestBase(private val base: String): BasePlatformTestCase() {
     override fun getTestDataPath(): String = "src/test/resources/quickfix"
 
     fun testTsxCreateKey() {
         val hint = "Create i18n key"
-        myFixture.configureByFiles("tsx/simple.tsx", "assets/test.json")
+        myFixture.configureByFiles("tsx/simple.tsx", "assets/test.$base")
         val action = myFixture.filterAvailableIntentions(hint).find {ac -> ac.text == hint}!!
         assertNotNull(action)
         myFixture.launchAction(action)
-        myFixture.checkResultByFile("assets/test.json", "assets/testExpected.json", false)
+        myFixture.checkResultByFile("assets/test.$base", "assets/testExpected.$base", false)
     }
 
     fun testTsxCreateKeyMultipleTranslations() {
-        myFixture.configureByFiles("tsx/simple.tsx", "assets/ru/test.json", "assets/en/test.json")
+        myFixture.configureByFiles("tsx/simple.tsx", "assets/ru/test.$base", "assets/en/test.$base")
         val action = myFixture.findSingleIntention("Create i18n key in all translation files")
         assertNotNull(action)
         myFixture.launchAction(action)
-        myFixture.checkResultByFile("assets/en/test.json", "assets/testExpected.json", false)
-        myFixture.checkResultByFile("assets/ru/test.json", "assets/testExpected.json", false)
+        myFixture.checkResultByFile("assets/en/test.$base", "assets/testExpected.$base", false)
+        myFixture.checkResultByFile("assets/ru/test.$base", "assets/testExpected.$base", false)
     }
 }
+
+internal class CreateKeyInJsonTest: CreateKeyTestBase("json")
+
+internal class CreateKeyInYamlTest: CreateKeyTestBase("yml")
