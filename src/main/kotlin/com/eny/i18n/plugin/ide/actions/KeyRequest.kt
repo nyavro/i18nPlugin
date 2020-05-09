f189d9ad1eda66c7289139baa626d89075f6a4fd
nyavro <e.nyavro@gmail.com>
2020-05-09 15:48:36 +0300

    Allow single-component keys like t('hello') for vue-i18n

--- src/main/kotlin/com/eny/i18n/plugin/ide/actions/KeyRequest.kt
+++ src/main/kotlin/com/eny/i18n/plugin/ide/actions/KeyRequest.kt
@@ -1,6 +1,7 @@
 package com.eny.i18n.plugin.ide.actions
 
-import com.eny.i18n.plugin.utils.ExpressionKeyParser
+import com.eny.i18n.plugin.ide.settings.Settings
+import com.eny.i18n.plugin.parser.ExpressionKeyParser
 import com.eny.i18n.plugin.utils.FullKey
 import com.eny.i18n.plugin.utils.KeyElement
 import com.intellij.openapi.project.Project
@@ -23,13 +24,19 @@ class KeyRequest {
      * Requests key
      */
     fun key(project: Project): KeyRequestResult {
+        val settings = Settings.getInstance(project)
         val keyStr = Messages
             .showInputDialog(project, "Specify the key", "Input i18n key", Messages.getQuestionIcon(), null, isValidKey())
         return if(keyStr == null) {
             KeyRequestResult(null, true)
         } else {
             KeyRequestResult(
-                parser.parse(listOf(KeyElement.literal(keyStr))),
+                parser.parse(listOf(KeyElement.literal(keyStr)),
+                    nsSeparator = settings.nsSeparator,
+                    keySeparator = settings.keySeparator,
+                    stopCharacters = settings.stopCharacters,
+                    emptyNamespace = settings.vue
+                ),
                 false
             )
         }
