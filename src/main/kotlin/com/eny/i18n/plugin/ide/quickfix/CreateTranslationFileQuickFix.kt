420fd9b19fa89517ad649b9584f8eaf0acf7b4b9
nyavro <e.nyavro@gmail.com>
2020-05-12 17:28:14 +0300

    Translation files creation made undoable

--- src/main/kotlin/com/eny/i18n/plugin/ide/quickfix/CreateTranslationFileQuickFix.kt
+++ src/main/kotlin/com/eny/i18n/plugin/ide/quickfix/CreateTranslationFileQuickFix.kt
@@ -2,6 +2,7 @@ package com.eny.i18n.plugin.ide.quickfix
 
 import com.eny.i18n.plugin.utils.FullKey
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFileFactory
@@ -20,16 +21,18 @@ class CreateTranslationFileQuickFix(
 
     override fun invoke(project: Project, editor: Editor) =
         ApplicationManager.getApplication().invokeLater {
-            val name: String = fileName + "." + contentGenerator.getFileType().defaultExtension
-            val content: String = contentGenerator.generateContent(fullKey, translationValue)
-            folderSelector.select { folders ->
-                ApplicationManager.getApplication().runWriteAction {
-                    folders.forEach { folder ->
-                        folder.add(
-                            PsiFileFactory.getInstance(project).createFileFromText(name, contentGenerator.getLanguage(), content)
-                        )
-                    }
+            doInvoke(project)
+        }
+
+    private fun doInvoke(project: Project) {
+        val name: String = fileName + "." + contentGenerator.getFileType().defaultExtension
+        val content: String = contentGenerator.generateContent(fullKey, translationValue)
+        folderSelector.select { folders ->
+            WriteCommandAction.runWriteCommandAction(project) {
+                folders.forEach {
+                    it.add(PsiFileFactory.getInstance(project).createFileFromText(name, contentGenerator.getLanguage(), content))
                 }
             }
         }
+    }
 }
