43f229f4f994eb1a4831561029be9689292380b0
nyavro <e.nyavro@gmail.com>
2020-05-14 06:01:40 +0300

    Unfinished code commented

--- src/main/kotlin/com/eny/i18n/plugin/tree/PsiElementTree.kt
+++ src/main/kotlin/com/eny/i18n/plugin/tree/PsiElementTree.kt
@@ -5,9 +5,6 @@ import com.intellij.json.JsonElementTypes
 import com.intellij.json.psi.JsonFile
 import com.intellij.json.psi.JsonObject
 import com.intellij.json.psi.JsonProperty
-import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
-import com.intellij.lang.javascript.psi.JSReferenceExpression
-import com.intellij.lang.javascript.psi.JSVariable
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.tree.TokenSet
@@ -26,7 +23,7 @@ abstract class PsiElementTree: Tree<PsiElement> {
          */
         fun create(file: PsiElement): PsiElementTree? =
             if (file is JsonFile) JsonElementTree.create(file)
-            else if (file is JSObjectLiteralExpression) JsElementTree.create(file)
+//            else if (file is JSObjectLiteralExpression) JsElementTree.create(file)
             else YamlElementTree.create(file)
     }
 }
@@ -60,48 +57,48 @@ class JsonElementTree(val element: PsiElement): PsiElementTree() {
 /**
  * Tree wrapper around js psi tree
  */
-class JsElementTree(val element: PsiElement): PsiElementTree() {
-    override fun value(): PsiElement = element
-    override fun isTree(): Boolean = element is JSObjectLiteralExpression
-    override fun findChild(name: String): Tree<PsiElement>? {
-        return (element as? JSObjectLiteralExpression)
-            ?.findProperty(name)
-            ?.value
-            ?.let { child ->
-                if (child is JSReferenceExpression) {
-                    resolveReferenceChain(child, 10)?.let {
-                        JsElementTree(it)
-                    }
-                } else {
-                    JsElementTree(child)
-                }
-            }
-    }
-    private fun resolveReferenceChain(element: JSReferenceExpression, depth: Int):JSObjectLiteralExpression? {
-        val res = element.reference?.resolve()
-        if (res is JSVariable) {
-            val rvalue = res.children[0]
-            if (rvalue is JSReferenceExpression && depth > 0) {
-                return resolveReferenceChain(rvalue, depth-1)
-            }
-            return rvalue as? JSObjectLiteralExpression
-        } else {
-            return res as? JSObjectLiteralExpression
-        }
-    }
-    override fun findChildren(regex: Regex): List<Tree<PsiElement>> {
-        return listOf(element.node)
-            .map { item -> item.firstChildNode.psi }
-            .filter { item -> item != null && item.text.unQuote().matches(regex) }
-            .map { item -> JsElementTree(item) }
-    }
-    companion object {
-        /**
-         * Creates instance of JsElementTree
-         */
-        fun create(file: PsiElement): JsElementTree? = JsElementTree(file)
-    }
-}
+//class JsElementTree(val element: PsiElement): PsiElementTree() {
+//    override fun value(): PsiElement = element
+//    override fun isTree(): Boolean = element is JSObjectLiteralExpression
+//    override fun findChild(name: String): Tree<PsiElement>? {
+//        return (element as? JSObjectLiteralExpression)
+//            ?.findProperty(name)
+//            ?.value
+//            ?.let { child ->
+//                if (child is JSReferenceExpression) {
+//                    resolveReferenceChain(child, 10)?.let {
+//                        JsElementTree(it)
+//                    }
+//                } else {
+//                    JsElementTree(child)
+//                }
+//            }
+//    }
+//    private fun resolveReferenceChain(element: JSReferenceExpression, depth: Int):JSObjectLiteralExpression? {
+//        val res = element.reference?.resolve()
+//        if (res is JSVariable) {
+//            val rvalue = res.children[0]
+//            if (rvalue is JSReferenceExpression && depth > 0) {
+//                return resolveReferenceChain(rvalue, depth-1)
+//            }
+//            return rvalue as? JSObjectLiteralExpression
+//        } else {
+//            return res as? JSObjectLiteralExpression
+//        }
+//    }
+//    override fun findChildren(regex: Regex): List<Tree<PsiElement>> {
+//        return listOf(element.node)
+//            .map { item -> item.firstChildNode.psi }
+//            .filter { item -> item != null && item.text.unQuote().matches(regex) }
+//            .map { item -> JsElementTree(item) }
+//    }
+//    companion object {
+//        /**
+//         * Creates instance of JsElementTree
+//         */
+//        fun create(file: PsiElement): JsElementTree? = JsElementTree(file)
+//    }
+//}
 
 
 /**
