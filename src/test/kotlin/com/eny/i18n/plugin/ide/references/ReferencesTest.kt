bc8e94925b9be2aa728bf22d854206091d73f308
nyavro <e.nyavro@gmail.com>
2020-05-15 06:31:22 +0300

    Multi language references from keys to translations test

--- src/test/kotlin/com/eny/i18n/plugin/ide/references/ReferencesTest.kt
+++ src/test/kotlin/com/eny/i18n/plugin/ide/references/ReferencesTest.kt
@@ -1,6 +1,7 @@
 package com.eny.i18n.plugin.ide.references
 
 import com.eny.i18n.plugin.utils.unQuote
+import com.intellij.psi.PsiElement
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 abstract class ReferencesTestBase(private val ext: String) : BasePlatformTestCase() {
@@ -13,6 +14,23 @@ abstract class ReferencesTestBase(private val ext: String) : BasePlatformTestCas
         assertNotNull(element)
         assertEquals("Reference in json", element!!.references[0].resolve()?.text?.unQuote())
     }
+
+    fun testMultiReference() {
+        myFixture.configureByFiles("jsx/multiReference.jsx", "assets/en/multi.$ext", "assets/de/multi.$ext")
+        val element = myFixture.file.findElementAt(myFixture.caretOffset)?.parent
+        assertNotNull(element)
+        assertEquals(setOf("Welcome", "Willkommen"), getResolvedValues(element))
+    }
+
+    fun testMultiReferenceDefNs() {
+        myFixture.configureByFiles("jsx/defNsmultiReference.jsx", "assets/en/translation.$ext", "assets/de/translation.$ext")
+        val element = myFixture.file.findElementAt(myFixture.caretOffset)?.parent
+        assertNotNull(element)
+        assertEquals(setOf("Welcome", "Willkommen"), getResolvedValues(element))
+    }
+
+    private fun getResolvedValues(element: PsiElement?) =
+        (element?.reference as I18nReference).references.map { it.element?.value()?.text?.unQuote() }.toSet()
 
     fun testDefaultNsReference() {
         myFixture.configureByFiles("jsx/defNsReference.jsx", "assets/translation.$ext")
---
+++ src/test/resources/references/jsx/defNsmultiReference.jsx
@@ -0,0 +1,3 @@
+export const test1 = () => {
+    return (<div>{i18n.t('main.header.title<caret>')}</div>);
+};
