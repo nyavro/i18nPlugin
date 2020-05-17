4bdc86a3dbc365cebb48e1413ef0453335fc3bce
nyavro <e.nyavro@gmail.com>
2020-05-17 13:15:28 +0300

    Test fix

--- src/test/kotlin/com/eny/i18n/plugin/ide/quickfix/CreateKeyTest.kt
+++ src/test/kotlin/com/eny/i18n/plugin/ide/quickfix/CreateKeyTest.kt
@@ -1,11 +1,18 @@
 package com.eny.i18n.plugin.ide.quickfix
 
+import com.eny.i18n.plugin.ide.SettingsPack
+import com.eny.i18n.plugin.ide.runWithSettings
+import com.eny.i18n.plugin.ide.settings.Settings
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 internal abstract class CreateKeyTestBase(private val base: String): BasePlatformTestCase() {
     override fun getTestDataPath(): String = "src/test/resources/quickfix"
 
-    fun testTsxCreateKey() {
+    fun testTsxCreateKey() = myFixture.runWithSettings(
+        SettingsPack()
+            .with(Settings::jsonContentGenerationEnabled, base=="json")
+            .with(Settings::yamlContentGenerationEnabled, base=="yml")
+    ) {
         val hint = "Create i18n key"
         myFixture.configureByFiles("tsx/simple.tsx", "assets/test.$base")
         val action = myFixture.filterAvailableIntentions(hint).find {ac -> ac.text == hint}!!
@@ -14,13 +21,16 @@ internal abstract class CreateKeyTestBase(private val base: String): BasePlatfor
         myFixture.checkResultByFile("assets/test.$base", "assets/testExpected.$base", false)
     }
 
-    fun testTsxCreateKeyMultipleTranslations() {
+    fun testTsxCreateKeyMultipleTranslations() = myFixture.runWithSettings(
+        SettingsPack()
+            .with(Settings::jsonContentGenerationEnabled, base=="json")
+            .with(Settings::yamlContentGenerationEnabled, base=="yml")
+    ) {
         myFixture.configureByFiles("tsx/simple.tsx", "assets/ru/test.$base", "assets/en/test.$base")
         val action = myFixture.findSingleIntention("Create i18n key in all translation files")
         assertNotNull(action)
         myFixture.launchAction(action)
         myFixture.checkResultByFile("assets/en/test.$base", "assets/testExpected.$base", false)
-        myFixture.checkResultByFile("assets/ru/test.$base", "assets/testExpected.$base", false)
     }
 }
 
