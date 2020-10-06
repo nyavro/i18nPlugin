fc392d955968a69afcd8e2e97d8d2f244ef783f2
nyavro <e.nyavro@gmail.com>
2020-10-07 01:54:18 +0700

    #48, #26 Vue sfc hint test

--- src/test/kotlin/com/eny/i18n/plugin/ide/hint/HintTest.kt
+++ src/test/kotlin/com/eny/i18n/plugin/ide/hint/HintTest.kt
@@ -2,21 +2,17 @@ package com.eny.i18n.plugin.ide.hint
 
 import com.eny.i18n.plugin.PlatformBaseTest
 import com.eny.i18n.plugin.ide.CodeTranslationGenerators
-import com.eny.i18n.plugin.ide.TranslationGenerators
-import com.eny.i18n.plugin.utils.generator.code.*
-import com.eny.i18n.plugin.utils.generator.translation.Json5TranslationGenerator
+import com.eny.i18n.plugin.ide.runVue
+import com.eny.i18n.plugin.utils.generator.code.CodeGenerator
+import com.eny.i18n.plugin.utils.generator.code.VueCodeGenerator
 import com.eny.i18n.plugin.utils.generator.translation.JsonTranslationGenerator
 import com.eny.i18n.plugin.utils.generator.translation.TranslationGenerator
-import com.eny.i18n.plugin.utils.generator.translation.YamlTranslationGenerator
 import com.intellij.codeInsight.documentation.DocumentationManager
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
-import org.junit.jupiter.params.provider.ValueSource
-import utils.randomOf
-import kotlin.concurrent.thread
 
-class JsHintTestBase: PlatformBaseTest() {
+class HintTest: PlatformBaseTest() {
 
     @ParameterizedTest
     @ArgumentsSource(CodeTranslationGenerators::class)
@@ -32,27 +28,24 @@ class JsHintTestBase: PlatformBaseTest() {
             )
         }
     }
-//
-//    @Test
-//    fun testVueSfcSingleHint() {
-//        val cg = VueCodeGenerator()
-//        val tg = JsonTranslationGenerator()
-//        val translation = "translation here"
-//        val translationBlock = tg.generateNamedBlock("en", tg.generateContent("root", "first", "second", translation))
-//        val generateSfcBlock = cg.generateSfcBlock(
-//            "<p>{{ \$t('root.first.<caret>second')}}</p>",
-//            translationBlock
-//        )
-//        myFixture.configureByText(
-//            "App.vue",
-//            generateSfcBlock
-//        )
-//        read {
-//            val codeElement = myFixture.file.findElementAt(myFixture.caretOffset)
-//            assertEquals(
-//                translation,
-//                DocumentationManager.getProviderFromElement(codeElement).getQuickNavigateInfo(codeElement, codeElement)
-//            )
-//        }
-//    }
+
+    @Test
+    fun vueSfcHint() = myFixture.runVue {
+        val cg = VueCodeGenerator()
+        val tg = JsonTranslationGenerator()
+        myFixture.configureByText(
+            "App.vue",
+            cg.generateSfcBlock(
+                "<p>{{ \$t('root.first.<caret>second')}}</p>",
+                tg.generateNamedBlock("en", tg.generateContent("root", "first", "second", "translation here"))
+            )
+        )
+        read {
+            val codeElement = myFixture.file.findElementAt(myFixture.caretOffset)
+            //Not supported for Vue SFC:
+            assertNull(
+                DocumentationManager.getProviderFromElement(codeElement).getQuickNavigateInfo(myFixture.elementAtCaret, codeElement)
+            )
+        }
+    }
 }
