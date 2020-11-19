9851d6944370bf74ee3e6cd983a8757108a62ef8
nyavro <e.nyavro@gmail.com>
2020-11-19 19:51:07 +0300

    Coverage up

--- src/test/kotlin/com/eny/i18n/plugin/ide/settings/ConfigurableTest.kt
+++ src/test/kotlin/com/eny/i18n/plugin/ide/settings/ConfigurableTest.kt
@@ -1,6 +1,8 @@
 package com.eny.i18n.plugin.ide.settings
 
 import com.eny.i18n.plugin.utils.PluginBundle
+import com.intellij.openapi.project.Project
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -10,7 +12,9 @@ class ConfigurableTest {
 
     @Test
     fun testConfigurable() {
-        val configurable = Configurable(mockk())
+        val project = mockk<Project>()
+        every { project.getService(Settings::class.java, true) } returns Settings()
+        val configurable = Configurable(project)
         assertEquals(PluginBundle.getMessage("app.name"), configurable.displayName)
         assertEquals("preference.i18nPlugin", configurable.helpTopic)
         assertEquals("preference.i18nPlugin", configurable.id)
