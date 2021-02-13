94cec9a9cc2ce9ef92f1451f90aece2e797a4619
nyavro <e.nyavro@gmail.com>
2021-02-13 19:25:02 +0700

    Disable unstable test

--- src/test/kotlin/com/eny/i18n/plugin/PlatformBaseTest.kt
+++ src/test/kotlin/com/eny/i18n/plugin/PlatformBaseTest.kt
@@ -3,4 +3,7 @@ package com.eny.i18n.plugin
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import org.junit.Test
 
-abstract class PlatformBaseTest: BasePlatformTestCase()
+abstract class PlatformBaseTest: BasePlatformTestCase() {
+    fun testEmpty() {
+    }
+}
