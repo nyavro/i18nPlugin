2be441b6f879949e5bab670fd4f1f8b96db0de2d
nyavro <e.nyavro@gmail.com>
2021-03-02 00:58:58 +0700

    Sample directory for 'Vue namespaces' feature

---
+++ examples/vue-namespaces/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>{{ $t('common.accept') }}</h1>
+    <h1>{{ $t('cita.price') }}</h1>
+    <div>{{title}}</div>
+  </div>
+</template>
+
+<style>
+  h1 {
+    color: #42b983;
+  }
+</style>
+<script>
+  export default {
+    name: "AppHeader",
+    computed: {
+      title() {
+        return this.$t('title');
+      }
+    }
+  }
+</script>
